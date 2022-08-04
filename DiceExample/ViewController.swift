a7b1851ded6dd3ac19939e21d9997a4726b475cb
YuGeonHui <ghyu_0906@naver.com>
2022-08-04 22:12:37 +0900

    commit

--- DiceExample/ViewController.swift
+++ DiceExample/ViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Dice: UILabel!
+    @IBOutlet weak var DiceImage: UIImageView!
+    @IBOutlet weak var StartButton: UIButton!
+    @IBOutlet weak var RollingButton: UIButton!
+    
+    private let DiceImages = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+    private let colors : [UIColor] = [.red, .green, .purple, .orange]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("커밋 시도하기")
+        Dice.textColor = .darkGray
+        Dice.text = "주사위를 돌려주세요"
+        Dice.textAlignment = .center
+        
+        StartButton.backgroundColor = .yellow
+        StartButton.layer.cornerRadius = 30
+        
+        DiceImage.image = UIImage(named: "DiceOne")
+        
     }
 
-
+    @IBAction func ClickButton(_ sender: Any) {
+        print("돌리는 중")
+        
+        let random = Int.random(in: 0...5)
+        DiceImage.image = DiceImages[random]
+        
+        Dice.text = (random + 1) % 2 == 0 ? "짝수입니다!" : "홀수입니다!"
+    }
+    
 }
 
